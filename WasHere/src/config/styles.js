116ec229413293f273c4cb1b6fbddf3a533bcab3
aldin-alagic <aldin@dysordian.com>
2021-03-14 01:38:12 +0100

    Add default styles and colors files into the config folder
    Added styles and colors will be used to standardize common styles and colors trough the project.

---
+++ WasHere/src/config/styles.js
@@ -0,0 +1,23 @@
+import { Platform } from "react-native";
+
+import colors from "./colors";
+
+export default {
+  colors,
+  text: {
+    color: colors.dark,
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+
+    elevation: 10,
+  }
+};
