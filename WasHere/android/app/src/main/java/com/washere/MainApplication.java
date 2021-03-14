cf3d4fdd59c044dd663ff47219c10361491d84e2
aldin-alagic <aldin@dysordian.com>
2021-03-14 06:11:55 +0100

    Change project files according to bottom-sheet additional installation instructions

--- WasHere/android/app/src/main/java/com/washere/MainApplication.java
+++ WasHere/android/app/src/main/java/com/washere/MainApplication.java
@@ -1,9 +1,12 @@
 package com.washere;
 
+import com.facebook.react.bridge.JSIModulePackage;
+ import com.swmansion.reanimated.ReanimatedJSIModulePackage; 
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -33,6 +36,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage(); // <- add
         }
       };
 
