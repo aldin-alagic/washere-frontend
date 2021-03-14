e7fc77f922e571b4b9ab6cf2f22166e9e928eb4a
aldin-alagic <aldin@dysordian.com>
2021-03-14 06:30:48 +0100

    Create a heading component for auth forms

---
+++ WasHere/src/components/form/Heading.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+import CloseButton from '../CloseButton';
+
+const Heading = ({title, onPress}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <CloseButton onPress={onPress} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems:"baseline"
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+});
+
+
+export default Heading;
