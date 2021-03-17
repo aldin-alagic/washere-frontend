d70c2f3949ecd7656e28db992cdf09b255fca090
fifi98 <bel.filip@hotmail.com>
2021-03-17 11:20:17 +0100

    Change shadow according to the prototype

--- WasHere/src/config/styles.js
+++ WasHere/src/config/styles.js
@@ -1,23 +1,23 @@
-import { Platform } from "react-native";
+import { Platform } from 'react-native';
 
-import colors from "./colors";
+import colors from './colors';
 
 export default {
   colors,
   text: {
     color: colors.dark,
     fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
   },
   shadow: {
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 5,
+      height: 2,
     },
-    shadowOpacity: 0.34,
-    shadowRadius: 6.27,
+    shadowOpacity: 0.35,
+    shadowRadius: 2.5,
 
     elevation: 10,
-  }
+  },
 };
