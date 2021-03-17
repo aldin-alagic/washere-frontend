466e9c6649bd07216d1b23baaeb886e97cf7c4fe
aldin-alagic <aldin@dysordian.com>
2021-03-17 21:55:14 +0100

    Create a Navigator component for fetching the token from redux store or secure storage and rendering the proper navigator component

---
+++ WasHere/src/navigation/Navigator.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import jwt_decode from 'jwt-decode';
+
+import AuthNavigator from './AuthNavigator';
+import AppNavigator from './AppNavigator';
+import { navigationRef } from './rootNavigation';
+import { initialStateSet } from '../store/auth';
+import authStorage from '../store/storage';
+
+const Navigator = ({ token }) => {
+  const dispatch = useDispatch();
+  const { token: storeToken } = useSelector((state) => state.auth);
+
+  // I had to do this here and not in App.js because App.js is not wrapped with the Store provider. This is a temp solution - I think that checks that follow can be made in a better way or in a different component... 
+  if (storeToken && !token) authStorage.storeToken(storeToken); // This will be triggered when the user has logged in and the app is re-rendering, token from the store is set in the storage
+  else if (token && !storeToken) dispatch(initialStateSet({ token, user: jwt_decode(token) })); // This will be triggered when the user has exited and started the app again, token from the storage is set in the store
+  
+  return <NavigationContainer ref={navigationRef}>{storeToken ? <AppNavigator /> : <AuthNavigator />}</NavigationContainer>;
+};
+
+export default Navigator;
