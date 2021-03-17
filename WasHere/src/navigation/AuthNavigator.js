f9d040f85a815c95db5cb099e71250d4a3957cb9
Jakov Glavina <cyber.shot.jakov@gmail.com>
2021-03-17 23:38:39 +0100

    Add ForgotPassword screen to AuthNavigator

--- WasHere/src/navigation/AuthNavigator.js
+++ WasHere/src/navigation/AuthNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Welcome from '../screens/Welcome';
 import Login from '../screens/Login';
+import ForgotPassword from '../screens/ForgotPassword/ForgotPassword';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ const AuthNavigator = () => (
   <Stack.Navigator headerMode="none" screenOptions={{ animationEnabled: false }}>
     <Stack.Screen name="Welcome" component={Welcome} />
     <Stack.Screen name="Login" component={Login} />
+    <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
   </Stack.Navigator>
 );
 
--- WasHere/src/navigation/routes.js
+++ WasHere/src/navigation/routes.js
@@ -2,4 +2,5 @@ export default Object.freeze({
   WELCOME: 'Welcome',
   LOGIN: 'Login',
   REGISTER: 'Register',
+  FORGOT_PASSWORD: 'ForgotPassword',
 });
