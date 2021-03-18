344176f288f86fac528f4a426cf48c50bf04ac64
fifi98 <bel.filip@hotmail.com>
2021-03-18 11:33:16 +0100

    Show placeholder posts on the Feed screen

--- WasHere/src/screens/Feed.js
+++ WasHere/src/screens/Feed.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+import Post from '../components/Post';
+
+import Screen from '../components/Screen';
+
+const POSTS = [
+  {
+    id: '1',
+    user: 'John Wick',
+    createdAt: '5 mins ago',
+  },
+  {
+    id: '2',
+    user: 'Jane Doe',
+    createdAt: '27 mins ago',
+  },
+  {
+    id: '3',
+    user: 'Nick Johnson',
+    createdAt: '1 hour ago',
+  },
+];
 
 const Feed = () => {
-  return <></>;
+  return (
+    <Screen style={styles.container}>
+      <FlatList data={POSTS} renderItem={({ item }) => <Post data={item} />} />
+    </Screen>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+});
 
 export default Feed;
