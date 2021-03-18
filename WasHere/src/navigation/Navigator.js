748c91eaae1d07016926d567a1fe52c16139c82e
fifi98 <bel.filip@hotmail.com>
2021-03-18 23:54:36 +0100

    Restructure auth workflow a bit

--- WasHere/src/navigation/Navigator.js
+++ WasHere/src/navigation/Navigator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import jwt_decode from 'jwt-decode';
@@ -7,17 +7,24 @@ import AuthNavigator from './AuthNavigator';
 import AppNavigator from './AppNavigator';
 import { initialStateSet } from '../store/auth';
 import authStorage from '../store/storage';
+import Loading from '../screens/Loading';
 
-const Navigator = ({ token }) => {
+const Navigator = () => {
   const dispatch = useDispatch();
-  const { token: storeToken } = useSelector((state) => state.auth);
+  const { token } = useSelector((state) => state.auth);
 
-  // I had to do this here and not in App.js because App.js is not wrapped with the Store provider. This is a temp solution - I think that checks that follow can be made in a better way or in a different component...
-  if (storeToken && !token) authStorage.storeToken(storeToken);
-  // This will be triggered when the user has logged in and the app is re-rendering, token from the store is set in the storage
-  else if (token && !storeToken) dispatch(initialStateSet({ token, user: jwt_decode(token) })); // This will be triggered when the user has exited and started the app again, token from the storage is set in the store
+  const [isReady, setIsReady] = useState(false);
 
-  return <NavigationContainer>{storeToken ? <AppNavigator /> : <AuthNavigator />}</NavigationContainer>;
+  useEffect(() => {
+    authStorage.getToken().then((token) => {
+      if (token) dispatch(initialStateSet({ token, user: jwt_decode(token) }));
+      setIsReady(true);
+    });
+  }, []);
+
+  if (!isReady) return <Loading />;
+
+  return <NavigationContainer>{token ? <AppNavigator /> : <AuthNavigator />}</NavigationContainer>;
 };
 
 export default Navigator;
