c31781b1bf4960a5ba4f10f0c3fb1edc90c8de77
fifi98 <bel.filip@hotmail.com>
2021-03-18 13:48:50 +0100

    Add random user photos to mockup data

--- WasHere/src/screens/Feed.js
+++ WasHere/src/screens/Feed.js
@@ -7,24 +7,45 @@ import Screen from '../components/Screen';
 const POSTS = [
   {
     id: '1',
-    name: 'John Wick',
-    createdAt: '5 mins ago',
-    latitude: 37.78825,
-    longitude: -122.4324,
+    user: {
+      name: 'John Wick',
+      photoURL: 'https://i.pravatar.cc/150?img=52',
+    },
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+    createdAt: '2021-03-18 13:15',
+    likes: 7,
+    comments: 5,
   },
   {
     id: '2',
-    name: 'Jane Doe',
-    createdAt: '27 mins ago',
-    latitude: 37.78825,
-    longitude: -122.4324,
+    user: {
+      name: 'Jane Doe',
+      photoURL: 'https://i.pravatar.cc/150?img=26',
+    },
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+    createdAt: '2021-03-18 13:05',
+    likes: 12,
+    comments: 6,
   },
   {
     id: '3',
-    name: 'Nick Johnson',
-    createdAt: '1 hour ago',
-    latitude: 37.78825,
-    longitude: -122.4324,
+    user: {
+      name: 'Carla Smith',
+      photoURL: 'https://i.pravatar.cc/150?img=27',
+    },
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+    createdAt: '2021-03-17 16:28',
+    likes: 4,
+    comments: 2,
   },
 ];
 
