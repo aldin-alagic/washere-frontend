681a6f3119e0eea493776e78b5347712647e0b25
aldin-alagic <aldin@dysordian.com>
2021-03-20 14:28:14 +0100

    Update Welcome component margins and paddings

--- WasHere/src/screens/Welcome.js
+++ WasHere/src/screens/Welcome.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, Dimensions } from 'react-native';
 
 import Screen from '../components/Screen';
 import AppButton from '../components/Button';
@@ -15,36 +15,32 @@ const Welcome = ({ navigation }) => {
       <Text style={styles.title}>WasHere</Text>
       <WelcomeScreenWhite style={styles.image} />
       <AppText style={styles.description}>
-        WasHere is a lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-        aliqua. Ut enim ad minim veniam
+        WasHere lets you connect with people based on locations that you visit. What are you waiting for? {'\n'} Start exploring!
       </AppText>
-      <View style={styles.buttonsContainer}>
-        <AppButton title="Continue with Facebook" color="facebook" onPress={() => navigation.navigate(routes.REGISTER)} />
-        <AppButton title="Continue with Google" color="google" onPress={() => navigation.navigate(routes.REGISTER)} />
-        <AppText style={styles.text}>or</AppText>
-        <AppButton title="Sign up with E-mail" color="white" textColor="black" onPress={() => navigation.navigate(routes.REGISTER)} />
-        <AppButton
-          text
-          textColor="black"
-          title={
-            <View style={styles.centered}>
-              <AppText>Already have a WasHere account?</AppText>
-              <AppText style={styles.underlined}>Sign in</AppText>
-            </View>
-          }
-          onPress={() => navigation.navigate(routes.LOGIN)}
-        />
-      </View>
+      <AppButton title="Continue with Facebook" color="facebook" onPress={() => navigation.navigate(routes.REGISTER)} />
+      <AppButton title="Continue with Google" color="google" onPress={() => navigation.navigate(routes.REGISTER)} />
+      <AppText style={styles.centered}>or</AppText>
+      <AppButton title="Sign up with E-mail" color="white" textColor="black" onPress={() => navigation.navigate(routes.REGISTER)} />
+      <AppButton
+        text
+        textColor="black"
+        title={
+          <View style={styles.centered}>
+            <AppText>Already have a WasHere account?</AppText>
+            <AppText style={styles.underlined}>Sign in</AppText>
+          </View>
+        }
+        onPress={() => navigation.navigate(routes.LOGIN)}
+      />
     </Screen>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'flex-start',
     backgroundColor: colors.white,
-    padding: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
   },
   title: {
     fontFamily: 'BalooBhai2-Medium',
@@ -52,28 +48,22 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: colors.primary,
   },
-  text: {
-    textAlign: 'center',
+  image: {
+    flexShrink: 1,
+    alignSelf: 'center',
   },
   description: {
     textAlign: 'center',
-    marginTop: 40,
-    marginBottom: 30,
-  },
-  underlined: {
-    textDecorationLine: 'underline',
+    marginTop: 10,
+    marginBottom: 20,
   },
   centered: {
-    alignItems: 'center',
+    textAlign: 'center',
   },
-  buttonsContainer: {
-    padding: 20,
-    width: '100%',
-  },
-  image: {
-    marginTop: 20,
-    marginBottom: 20,
+  underlined: {
+    textAlign: 'center',
     alignSelf: 'center',
+    textDecorationLine: 'underline',
   },
 });
 
