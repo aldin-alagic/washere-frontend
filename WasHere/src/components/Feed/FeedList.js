be715417ca5c8e0499a6943ec04c917b7620264e
Jakov Glavina <cyber.shot.jakov@gmail.com>
2021-04-18 14:54:05 +0200

    Refactor Feed FlatList component to make it reusable

---
+++ WasHere/src/components/Feed/FeedList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FlatList } from "react-native";
+
+import Post from "../Post/";
+
+const FeedList = ({ items, ...props }) => {
+  return <FlatList {...props} data={items} renderItem={({ item }) => <Post data={item} />} />;
+};
+
+export default FeedList;
