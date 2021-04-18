10ba9d1626e7626e78c4fb5eb9e04c59de31d0d5
Jakov Glavina <cyber.shot.jakov@gmail.com>
2021-04-18 23:28:57 +0200

    Use smaller safe area insets on iOS

--- WasHere/src/components/Screen.js
+++ WasHere/src/components/Screen.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { StyleSheet, View, StatusBar, Platform } from 'react-native';
+import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { StyleSheet, View, StatusBar, Platform } from "react-native";
 
 const Screen = ({ children, style, noTopPadding }) => {
   const insets = useSafeAreaInsets();
   return (
     <View
-      style={[styles.screen, style, { paddingTop: noTopPadding ? 0 : Platform.OS === 'android' ? StatusBar.currentHeight : insets.top }]}>
+      style={[
+        styles.screen,
+        style,
+        { paddingTop: noTopPadding ? 0 : Platform.OS === "android" ? StatusBar.currentHeight : insets.top - 24 },
+      ]}>
       <View style={[styles.view, style]}>{children}</View>
     </View>
   );
