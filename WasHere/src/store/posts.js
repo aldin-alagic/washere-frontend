aa06f10620ccc72aa361d27b48b4e5c916be124a
aldin-alagic <aldin@dysordian.com>
2021-04-19 04:32:18 +0200

    Create Posts Redux Slice

---
+++ WasHere/src/store/posts.js
@@ -0,0 +1,55 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { showMessage } from "react-native-flash-message";
+
+import { apiCallBegan } from "./api";
+import * as RootNavigation from "../navigation/RootNavigation";
+import routes from "../navigation/routes";
+
+import { API_ERROR_MESSAGE } from "../config/config.json";
+
+const slice = createSlice({
+  name: "posts",
+  initialState: {},
+  reducers: {
+    requestStarted: (auth, action) => {
+      auth.loading = true;
+    },
+
+    postCreated: (auth, action) => {
+      const { success, message } = action.payload;
+      auth.loading = false;
+      showMessage({
+        message: success ? "Success!" : "Error!",
+        description: message,
+        type: success ? "success" : "warning",
+        autoHide: true,
+      });
+      if (success) {
+        RootNavigation.navigate(routes.MAP);
+      }
+    },
+
+    requestFailed: (auth, action) => {
+      auth.loading = false;
+      showMessage({
+        message: API_ERROR_MESSAGE,
+        description: action.payload,
+        type: "warning",
+        autoHide: true,
+      });
+    },
+  },
+});
+
+export const { requestStarted, postCreated, requestFailed } = slice.actions;
+export default slice.reducer;
+
+export const createPost = (description, isPublic, latitude, longitude) =>
+  apiCallBegan({
+    url: "/post",
+    method: "POST",
+    data: { description, is_public: isPublic, latitude, longitude },
+    onStart: requestStarted.type,
+    onSuccess: postCreated.type,
+    onError: requestFailed.type,
+  });
