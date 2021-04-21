2099f35cd67cfee064659dfe2e5ed62c673bbea8
aldin-alagic <aldin@dysordian.com>
2021-04-21 04:16:00 +0200

    Add textAlignVertical style to for multiline TextInput

--- WasHere/src/components/TextInput.js
+++ WasHere/src/components/TextInput.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
-import defaultStyles from '../config/styles';
+import defaultStyles from "../config/styles";
 
-const AppTextInput = ({ icon, width = '100%', ...otherProps }) => {
+const AppTextInput = ({ icon, width = "100%", ...otherProps }) => {
   return (
     <View style={[styles.container, { width }]}>
       {icon && <Icon name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon} />}
@@ -17,19 +17,20 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: defaultStyles.colors.light,
     borderRadius: 15,
-    flexDirection: 'row',
+    flexDirection: "row",
+    alignItems: "center",
     padding: 6,
     marginVertical: 10,
-    alignItems: 'center',
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   icon: {
     marginHorizontal: 10,
   },
   textInput: {
     paddingVertical: 4,
-    width: '100%',
-    fontWeight: '100',
+    width: "100%",
+    fontWeight: "100",
+    textAlignVertical: "top",
   },
 });
 
