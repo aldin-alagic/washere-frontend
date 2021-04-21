de4f0b41cdbaa9077e32d35576c06e7bdd33bf8e
aldin-alagic <aldin@dysordian.com>
2021-04-21 04:16:42 +0200

    Remove unecessary multiline property for FormField

--- WasHere/src/components/form/FormField.js
+++ WasHere/src/components/form/FormField.js
@@ -4,12 +4,12 @@ import { useFormikContext } from 'formik';
 import TextInput from '../TextInput';
 import ErrorMessage from './ErrorMessage';
 
-const AppFormField = ({ name, width, multiline, ...otherProps }) => {
+const AppFormField = ({ name, width, ...otherProps }) => {
   const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
 
   return (
     <>
-      <TextInput multiline={multiline} onBlur={() => setFieldTouched(name)} onChangeText={handleChange(name)} width={width} {...otherProps} />
+      <TextInput onBlur={() => setFieldTouched(name)} onChangeText={handleChange(name)} width={width} {...otherProps} />
       <ErrorMessage error={errors[name]} visible={touched[name]} />
     </>
   );
