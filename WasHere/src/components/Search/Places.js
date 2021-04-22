9048c00687a81dd55246fe66910264c77db87d0e
Jakov Glavina <cyber.shot.jakov@gmail.com>
2021-04-22 23:29:01 +0200

    Search screeen - Recents & Places tabs with mock data

--- WasHere/src/components/Search/Places.js
+++ WasHere/src/components/Search/Places.js
@@ -1,6 +1,47 @@
 import React from "react";
-import { View } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 
-const Places = () => <View style={{ flex: 1, backgroundColor: "#ff4081" }} />;
+import Place from "./Place";
+import colors from "../../config/colors";
+
+const places = [
+  {
+    id: "1",
+    name: "Pizza Hut",
+    address: "Ul. grada Vukovara 295, 10000 Zagreb",
+    image: "https://i.pravatar.cc/150?img=26",
+  },
+  {
+    id: "2",
+    name: "Franko's Pizza & Bar",
+    address: "Ul. Kneza Branimira 71a, 10000 Zagreb",
+    image: "https://i.pravatar.cc/150?img=26",
+  },
+  {
+    id: "3",
+    name: "Omma Korean Restaurant",
+    address: "Unska ul. 28, 10000 Zagreb",
+    image: "https://i.pravatar.cc/150?img=26",
+  },
+  {
+    id: "4",
+    name: "Burger Bar Zagreb",
+    address: "Avenija Dubrovnik 15ZV, 10000 Zagreb",
+    image: "https://i.pravatar.cc/150?img=26",
+  },
+];
+
+const Places = () => (
+  <View style={styles.container}>
+    <FlatList data={places} renderItem={({ item }) => <Place data={item} />} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+});
 
 export default Places;
--- WasHere/src/components/Search/Recent.js
+++ WasHere/src/components/Search/Recent.js
@@ -1,6 +1,60 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import FeedList from "../Feed/FeedList";
 
-const Recent = () => <View style={{ flex: 1, backgroundColor: "#ff4081" }} />;
+const posts = [
+  {
+    id: "1",
+    user: {
+      name: "Carla Smith",
+      photoURL: "https://i.pravatar.cc/150?img=26",
+    },
+    location: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+    },
+    createdAt: "2021-03-18 13:15",
+    likes: 7,
+    comments: 5,
+  },
+  {
+    id: "2",
+    user: {
+      name: "Carla Smith",
+      photoURL: "https://i.pravatar.cc/150?img=26",
+    },
+    location: {
+      latitude: 37.78025,
+      longitude: -122.4524,
+    },
+    createdAt: "2021-03-18 13:05",
+    likes: 12,
+    comments: 6,
+  },
+  {
+    id: "3",
+    user: {
+      name: "Carla Smith",
+      photoURL: "https://i.pravatar.cc/150?img=27",
+    },
+    location: {
+      latitude: 37.78225,
+      longitude: -122.4824,
+    },
+    createdAt: "2021-03-17 16:28",
+    likes: 4,
+    comments: 2,
+  },
+];
+
+const Recent = () => (
+  <View style={styles.container}>
+    <FeedList items={posts} showsVerticalScrollIndicator={false} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: { paddingHorizontal: 10, paddingTop: 15 },
+});
 
 export default Recent;
