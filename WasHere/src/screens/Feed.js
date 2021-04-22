dfedf90555ed5b49742bd5a3cfff7ec9fe4ad78b
fifi98 <filip.bel@hotmail.com>
2021-04-23 00:50:05 +0200

    Load feed from API

--- WasHere/src/screens/Feed.js
+++ WasHere/src/screens/Feed.js
@@ -1,57 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
 import FeedList from "../components/Feed/FeedList";
 import Screen from "../components/Screen";
+import { getFeed } from "../store/posts";
 
 import colors from "../config/colors";
 
-const posts = [
-  {
-    id: "1",
-    user: {
-      name: "John Wick",
-      photoURL: "https://i.pravatar.cc/150?img=52",
-    },
-    location: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-    },
-    createdAt: "2021-03-18 13:15",
-    likes: 7,
-    comments: 5,
-  },
-  {
-    id: "2",
-    user: {
-      name: "Jane Doe",
-      photoURL: "https://i.pravatar.cc/150?img=26",
-    },
-    location: {
-      latitude: 37.78025,
-      longitude: -122.4524,
-    },
-    createdAt: "2021-03-18 13:05",
-    likes: 12,
-    comments: 6,
-  },
-  {
-    id: "3",
-    user: {
-      name: "Carla Smith",
-      photoURL: "https://i.pravatar.cc/150?img=27",
-    },
-    location: {
-      latitude: 37.78225,
-      longitude: -122.4824,
-    },
-    createdAt: "2021-03-17 16:28",
-    likes: 4,
-    comments: 2,
-  },
-];
-
 const Feed = () => {
+  const posts = useSelector((state) => state.posts.feed);
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(getFeed()), []);
+
   return (
     <Screen style={styles.container}>
       <FeedList items={posts} />
