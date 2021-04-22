f27fe4f1f7db493d344f95356b74c39d377eace9
fifi98 <filip.bel@hotmail.com>
2021-04-23 00:50:39 +0200

    Add feed array to store the user's feed

--- WasHere/src/store/reducer.js
+++ WasHere/src/store/reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import authReducer from "./auth";
 import userReducer from "./user";
+import postsReducer from "./posts";
 
 export default combineReducers({
   auth: authReducer,
   user: userReducer,
+  posts: postsReducer,
 });
