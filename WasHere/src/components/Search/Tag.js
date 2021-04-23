35a74dc9dc95e411bd7dbc833b3c197fc80c7748
Jakov Glavina <cyber.shot.jakov@gmail.com>
2021-04-23 22:48:58 +0200

    Search screen - tags tab view layout

---
+++ WasHere/src/components/Search/Tag.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { View, StyleSheet, TouchableOpacity, Text, Image } from "react-native";
+import colors from "../../config/colors";
+import Hash from "../../assets/images/hash.svg";
+
+const Place = ({ data }) => {
+  console.log("DATA", data);
+  return (
+    <View>
+      <TouchableOpacity
+        style={styles.container}
+        onPress={() => {
+          console.log("PRESSED TAG");
+        }}>
+        <View style={styles.hash}>
+          <Hash style={styles.hashIcon} />
+        </View>
+
+        <Text style={styles.tagValue}>{data.value}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  hash: {
+    width: 55,
+    height: 55,
+    borderRadius: 50,
+    backgroundColor: colors.primary,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 10,
+  },
+
+  hashIcon: {
+    width: 30,
+    height: 30,
+  },
+  tagValue: {
+    fontWeight: "bold",
+    color: colors.primary,
+    marginBottom: 2,
+  },
+  placeAddress: {
+    color: colors.mediumlight,
+  },
+  image: {
+    borderRadius: 50,
+    height: 60,
+    width: 60,
+    marginRight: 10,
+  },
+});
+
+export default Place;
--- WasHere/src/components/Search/Tags.js
+++ WasHere/src/components/Search/Tags.js
@@ -1,6 +1,39 @@
 import React from "react";
-import { View } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 
-const Tags = () => <View style={{ flex: 1, backgroundColor: "#ff4081" }} />;
+import Tag from "./Tag";
+import colors from "../../config/colors";
+
+const tags = [
+  {
+    id: "1",
+    value: "#gym",
+  },
+  {
+    id: "2",
+    value: "#food",
+  },
+  {
+    id: "3",
+    value: "#park",
+  },
+  {
+    id: "4",
+    value: "#casual",
+  },
+];
+
+const Tags = () => (
+  <View style={styles.container}>
+    <FlatList data={tags} renderItem={({ item }) => <Tag data={item} />} />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+});
 
 export default Tags;
