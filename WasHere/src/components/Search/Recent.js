613d1af6a10f796a117899d8247a601d94d9a17e
Jakov Glavina <cyber.shot.jakov@gmail.com>
2021-04-24 23:57:49 +0200

    Recents tab - Show posts from backend instead of mock data

--- WasHere/src/components/Search/Recent.js
+++ WasHere/src/components/Search/Recent.js
@@ -1,57 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, StyleSheet } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
 import FeedList from "../Feed/FeedList";
+import { getFeed } from "../../store/posts";
 
-const posts = [
-  {
-    id: "1",
-    user: {
-      name: "Carla Smith",
-      photoURL: "https://i.pravatar.cc/150?img=26",
-    },
-    location: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-    },
-    createdAt: "2021-03-18 13:15",
-    likes: 7,
-    comments: 5,
-  },
-  {
-    id: "2",
-    user: {
-      name: "Carla Smith",
-      photoURL: "https://i.pravatar.cc/150?img=26",
-    },
-    location: {
-      latitude: 37.78025,
-      longitude: -122.4524,
-    },
-    createdAt: "2021-03-18 13:05",
-    likes: 12,
-    comments: 6,
-  },
-  {
-    id: "3",
-    user: {
-      name: "Carla Smith",
-      photoURL: "https://i.pravatar.cc/150?img=27",
-    },
-    location: {
-      latitude: 37.78225,
-      longitude: -122.4824,
-    },
-    createdAt: "2021-03-17 16:28",
-    likes: 4,
-    comments: 2,
-  },
-];
+const Recent = () => {
+  const { feed, loading } = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
 
-const Recent = () => (
-  <View style={styles.container}>
-    <FeedList items={posts} showsVerticalScrollIndicator={false} />
-  </View>
-);
+  useEffect(() => dispatch(getFeed()), []);
+  return (
+    <View style={styles.container}>
+      <FeedList
+        items={feed.posts}
+        onRefresh={() => dispatch(getFeed())}
+        refreshing={loading}
+        onEndReached={() => dispatch(getFeed(feed.lastPostId))}
+        onEndReachedThreshold={0.8}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: { paddingHorizontal: 10, paddingTop: 15 },
