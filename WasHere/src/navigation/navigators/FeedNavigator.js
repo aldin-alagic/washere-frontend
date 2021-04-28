c0e7966f042ca8076eea9cb54e1f87c09098bb1a
fifi98 <filip.bel@hotmail.com>
2021-04-28 22:03:39 +0200

    Change feed screen header title

--- WasHere/src/navigation/navigators/FeedNavigator.js
+++ WasHere/src/navigation/navigators/FeedNavigator.js
@@ -9,7 +9,7 @@ const FeedStack = createStackNavigator();
 const Feed = () => {
   return (
     <FeedStack.Navigator mode="modal" screenOptions={navigatorOptions}>
-      <FeedStack.Screen name="Search" component={FeedScreen} options={stackScreenOptions} />
+      <FeedStack.Screen name="Search" component={FeedScreen} options={{ ...stackScreenOptions, title: "Feed" }} />
     </FeedStack.Navigator>
   );
 };
