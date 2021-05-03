072d71ee89bace53d1eaf406a1398afda9e6ccbc
fifi98 <filip.bel@hotmail.com>
2021-05-03 23:16:39 +0200

    Add marginTop

--- WasHere/src/screens/user/Profile.js
+++ WasHere/src/screens/user/Profile.js
@@ -39,68 +39,65 @@ const Profile = ({ route, navigation }) => {
   return (
     <>
       <Screen style={styles.container}>
-        <View style={{ marginTop: 15 }}>
-          <FlatList
-            data={profile.posts}
-            ListHeaderComponent={
-              <View>
-                <View style={styles.basicInformation}>
-                  <ProfilePhoto size={100} photoKey={profile.user.profile_photo} />
-                  <View style={styles.textInformation}>
-                    <Text style={styles.username}>@{profile.user.username}</Text>
-                    <Text style={styles.text}>{profile.user.about}</Text>
-                  </View>
+        <FlatList
+          data={profile.posts}
+          ListHeaderComponent={
+            <View style={{ marginTop: 15 }}>
+              <View style={styles.basicInformation}>
+                <ProfilePhoto size={100} photoKey={profile.user.profile_photo} />
+                <View style={styles.textInformation}>
+                  <Text style={styles.username}>@{profile.user.username}</Text>
+                  <Text style={styles.text}>{profile.user.about}</Text>
                 </View>
-
-                {profile.mutualConnections.length !== 0 && (
-                  <>
-                    <Text style={[{ color: colors.mediumlight, marginTop: 10 }, styles.text]}>Mutual connections</Text>
-                    <View style={styles.connections}>
-                      <FlatList
-                        style={styles.connectionsList}
-                        horizontal={true}
-                        data={profile.mutualConnections}
-                        renderItem={({ item }) => <ConnectionSimple data={item} />}
-                      />
-                    </View>
-                    <View style={styles.divider} />
-                  </>
-                )}
-
-                {!profile.connected && (
-                  <View>
-                    <BlankSpacer height={8} />
-                    <Text style={[{ color: colors.mediumlight }, styles.text]}>Mutual places visited</Text>
-                    <PlacesCarousel
-                      places={[
-                        { id: 1, latitude: 43.123, longitude: 44.321 },
-                        { id: 2, latitude: 43.123, longitude: 44.321 },
-                      ]}
-                    />
-                    <View style={styles.divider} />
-                  </View>
-                )}
-                {!profile.connected && (
-                  <View style={{ marginTop: 6, marginBottom: 10 }}>
-                    <AppButton
-                      title={profile.requestSent ? "Pending approval" : "Connect"}
-                      onPress={() => dispatch(requestConnection(profileId))}
-                      color={profile.requestSent ? "mediumlight" : "primary"}
-                      customStyle={{ padding: 9 }}
-                    />
-                  </View>
-                )}
-                {!profile.connected && (
-                  <Text style={[{ color: colors.mediumlight, lineHeight: 17 }, styles.text]}>
-                    Connect with {profile.user.fullname} in order to be able to see her contact information and places where she has been
-                    to.
-                  </Text>
-                )}
               </View>
-            }
-            renderItem={({ item }) => <Post data={item} />}
-          />
-        </View>
+
+              {profile.mutualConnections.length !== 0 && (
+                <>
+                  <Text style={[{ color: colors.mediumlight, marginTop: 10 }, styles.text]}>Mutual connections</Text>
+                  <View style={styles.connections}>
+                    <FlatList
+                      style={styles.connectionsList}
+                      horizontal={true}
+                      data={profile.mutualConnections}
+                      renderItem={({ item }) => <ConnectionSimple data={item} />}
+                    />
+                  </View>
+                  <View style={styles.divider} />
+                </>
+              )}
+
+              {!profile.connected && (
+                <View>
+                  <BlankSpacer height={8} />
+                  <Text style={[{ color: colors.mediumlight }, styles.text]}>Mutual places visited</Text>
+                  <PlacesCarousel
+                    places={[
+                      { id: 1, latitude: 43.123, longitude: 44.321 },
+                      { id: 2, latitude: 43.123, longitude: 44.321 },
+                    ]}
+                  />
+                  <View style={styles.divider} />
+                </View>
+              )}
+              {!profile.connected && (
+                <View style={{ marginTop: 6, marginBottom: 10 }}>
+                  <AppButton
+                    title={profile.requestSent ? "Pending approval" : "Connect"}
+                    onPress={() => dispatch(requestConnection(profileId))}
+                    color={profile.requestSent ? "mediumlight" : "primary"}
+                    customStyle={{ padding: 9 }}
+                  />
+                </View>
+              )}
+              {!profile.connected && (
+                <Text style={[{ color: colors.mediumlight, lineHeight: 17 }, styles.text]}>
+                  Connect with {profile.user.fullname} in order to be able to see her contact information and places where she has been to.
+                </Text>
+              )}
+            </View>
+          }
+          renderItem={({ item }) => <Post data={item} />}
+        />
       </Screen>
       <BottomSheet
         bottomSheetRef={editProfileRef}
